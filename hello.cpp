0fd0475138b6ad8183f905308d3ca23e826b48bf
Edson Augusto <edsonaugustotl@gmail.com>
2023-10-02 14:04:51 -0300

    first project in c++

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	//double(number including decimal
+	double x = 2.5;
+	
+	// char (single cahr)
+	char a = 'a';
+	char b = 'b';
+
+	// boolean (true or false)
+	
+ 	bool c = true;	
+
+
+
+	std::cout << x << std::endl;
+	return 0;
+}
